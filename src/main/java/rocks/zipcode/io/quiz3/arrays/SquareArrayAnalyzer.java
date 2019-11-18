b7303f3c68bb25f7883ff98932cc3b8cf8c8f3db
Mike King <michkin32@gmail.com>
2019-11-18 14:41:14 -0500

    some arrays done

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -1,5 +1,8 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * @author leon on 09/12/2018.
  */
@@ -14,6 +17,27 @@ public class SquareArrayAnalyzer {
      * @return true if each `inputArray[i]` has a corresponding `squaredValues[j]` such that `squaredValues[j] == inputArray[i] * inputArray[i]`
      */
     public static Boolean compare(Integer[] inputArray, Integer[] squaredValues) {
-        return null;
+//        Integer[] secondArray = new Integer[squaredValues.length];
+        for (int i = 0; i < squaredValues.length -1; i++){
+            for(int j = 1; j < squaredValues.length -1; j++){
+                Integer hold = squaredValues[i];
+                if (squaredValues[i] < squaredValues[j]){
+                    squaredValues[i] = squaredValues[j];
+                    squaredValues[j] = hold;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < inputArray.length -1; i++){
+            if (inputArray[i] != (squaredValues[i] * squaredValues[i])) {
+                return false;
+            }
+            }
+
+        return true;
+    }
+    public void sorter(){
+
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -4,34 +4,60 @@ package rocks.zipcode.io.quiz3.arrays;
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
+    String[][] board;
 
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        String[] stringArray = new String[3];
+        for (int i =0; i < stringArray.length; i++){
+            stringArray[i] = board[value][i];
+        }
+        return stringArray;
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        String[] stringArray = new String[3];
+        for (int i =0; i < stringArray.length; i++){
+            stringArray[i] = board[i][value];
+        }
+        return stringArray;
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+
+            if (board[rowIndex][0].equals(board[rowIndex][1]) && board[rowIndex][0].equals(board[rowIndex][2])){
+                return true;
+            }
+
+        return false;
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+        if (board[0][columnIndex].equals(board[1][columnIndex]) && board[0][columnIndex].equals(board[2][columnIndex])){
+            return true;
+        }
+
+        return false;
     }
 
     public String getWinner() {
-        return null;
+        for (int i =0; i < board.length -1; i++){
+            if(isColumnHomogeneous(i)){
+                return board[i][0];
+            }else if (isRowHomogenous(i)){
+                return board[0][i];
+            }
+        }
+        return "Tie";
     }
 
     public String[][] getBoard() {
-        return null;
+        return board;
     }
 }
